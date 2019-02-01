1ec1250e2ed928de0f7de68606be2930cee8bdf4
zhangyu <450930750@qq.com>
2019-02-01 09:13:35 +0800

    实现全局设置，菜品类别

--- src/views/TableAdd.vue
+++ src/views/TableAdd.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="login">
-    <h1>TableAdd</h1>
+    <el-breadcrumb>
+      <el-breadcrumb-item to="/mian">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>桌台管理</el-breadcrumb-item>
+      <el-breadcrumb-item>添加桌台</el-breadcrumb-item>
+    </el-breadcrumb>
+    <br>
+    <el-card shadow="never">
+      <el-form label-width="110px">
+        <el-form-item label="桌台编号：">
+          <el-input v-model="formData.appName"></el-input>
+        </el-form-item>
+        <el-form-item label="桌台别名：">
+          <el-input v-model="formData.apiUrl"></el-input>
+        </el-form-item>
+        <el-form-item label="桌台类型：">
+          <el-input v-model="formData.apiUrl"></el-input>
+        </el-form-item>
+        
+        <el-form-item>
+          <el-button type="primary">提交</el-button>
+          <el-button>取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
   </div>
 </template>
--- src/views/TableList.vue
+++ src/views/TableList.vue
@@ -1,5 +1,39 @@
 <template>
-  <div class="login">
-    <h1>TableList</h1>
+  <div class="xfn-table-list">
+    <el-breadcrumb>
+      <el-breadcrumb-item to="/main">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>桌面管理</el-breadcrumb-item>
+      <el-breadcrumb-item>桌台列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <br>
+    <el-row>
+      <el-col v-bind:key="(t,i)" v-for="(t,i) in tableList" :xs='12' :sm="8" :md="6" :lg="4" :xl="2">餐桌</el-col>
+    </el-row>
   </div>
 </template>
+
+
+<script>
+import Table from "../components/Table";
+export default {
+  data() {
+    return {
+      tableList: []
+    };
+  },
+  components: {
+    "xfn-table": Table
+  },
+  created() {
+    var url = this.$store.state.globalSettings.apiUrl + "/admin/table";
+    this.$axios
+      .get(url)
+      .then(res => {
+        this.tableList = res.data;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+};
+</script>
